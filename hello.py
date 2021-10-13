adc2f3fc172c59f7d5ec77960791b621e487913e
Your Name <you@example.com>
2021-10-13 08:59:04 +0200

    author

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+# Mein hello - programm
+# (c) philipp.thalmeier@ms.bulme.at
 import datetime
 d = datetime.datetime.now()
 print("Guten Start ins neue Schuljahr!")
 n = input("Wie ist dein Name?\n")
 print(f' Guten Tag {n}!\n Es ist {d}')
-print("*********************")
+print("***********************************")
